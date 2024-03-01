3424142c0f9f78eaa49da3ecf3639c33d04c96f4
Jonas <publik.froeller.jonas@gmx.at>
2024-03-02 00:00:13 +0100

    feat(root.layout): added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
+	import { inject as injectAnalytics } from '@vercel/analytics';
 	import '$main/app.css';
 
 	injectSpeedInsights();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
