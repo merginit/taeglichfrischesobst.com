8c922a202c61ea04ee2864e7cb3ddb05367297a0
Jonas <publik.froeller.jonas@gmx.at>
2025-06-07 17:12:09 +0200

    adds creator note

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -19,7 +19,7 @@
 
 	{#if outerWidth > 1435}
 		<!-- prettier-ignore -->
-		<div id="streaming" class="absolute bottom-0 right-0 z-40 w-full">
+		<div id="streaming" class="absolute right-0 bottom-0 z-40 w-full">
 			<div class="flex flex-row-reverse gap-2 m-4 mb-8">
 				<IconLoader anchor_href={"https://open.spotify.com/artist/1dnEfTWZekuLgNFkASxQqV"} anchor_classes={"transition-opacity hover:opacity-90"} icon="mdi:spotify" color="hsl(var(--s))">spotify</IconLoader>
 				<IconLoader anchor_href={"https://music.amazon.de/artists/B0BBSY4YP1/t%C3%A4glich-frisches-obst?marketplaceId=A1PA6795UKMFR9&musicTerritory=DE&ref=dm_sh_AhKPMyUQ5RtLmXouGyIV6Uqxm"} anchor_classes={"transition-opacity hover:opacity-90"} icon="arcticons:amazon-music" color="hsl(var(--s))">amazon music</IconLoader>
@@ -29,6 +29,12 @@
 			</div>
 		</div>
 	{/if}
+
+	<div class="relative z-30 pb-4 text-center">
+		<p class="text-sm text-base-content/60">
+			Website erstellt von <a href="https://merginit.com" target="_blank" rel="noopener" class="link link-hover">Jonas Fröller / MerginIT e.U.</a>
+		</p>
+	</div>
 
 	<!-- prettier-ignore -->
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path class="pointer-events-none fill-neutral" fill-opacity="1" d="M0,128L60,160C120,192,240,256,360,250.7C480,245,600,171,720,165.3C840,160,960,224,1080,234.7C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
